8ed549f75dab13f74eaae75b83316814d3c4f187
CamilaRaimundo <camila.raimundo@unesp.br>
2023-05-10 21:59:25 -0300

    10.05.2023
    Criação do repositório e adição dos arquivos.

---
+++ psique_cti/resources/views/layout/_cabecalho.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="../../css/app.css">
+    <link rel="shortcut icon" href="images/icone_cf.png">
+
+    <!-- icones -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://kit.fontawesome.com/58fe79a519.js" crossorigin="anonymous"></script>
+
+</head>
---
+++ psique_cti/resources/views/layout/_rodape.blade.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="nav">
+            <a href="index.html">Home</a>
+            <a href="paginas/agenda.html">Agenda</a>
+            <a href="paginas/mural.html">Mural</a>
+        </div>
+    </footer>
+
+</body>
+</html>
