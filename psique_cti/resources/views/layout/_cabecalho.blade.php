2023058f969617292a67543a5d627f8bdc2dd251
CamilaRaimundo <camila.raimundo@unesp.br>
2023-08-26 12:00:13 -0300

    26.08.2023
    Colocação do Dark Mode.
    
    Co-Authored-By: Joao Pedro Rahal <134441287+JoaoPedroRahal@users.noreply.github.com>
    Co-Authored-By: Karine08 <114179037+Karine08@users.noreply.github.com>
    Co-Authored-By: LManuu <113178843+LManuu@users.noreply.github.com>
    Co-Authored-By: isabelliclaro <114405583+isabelliclaro@users.noreply.github.com>
    Co-Authored-By: lustangher <114400150+lustangher@users.noreply.github.com>

--- psique_cti/resources/views/layout/_cabecalho.blade.php
+++ psique_cti/resources/views/layout/_cabecalho.blade.php
@@ -49,32 +49,6 @@
   }
 </script>
 
-  {{-- Dark mode --}}
-  <script>
-    const toggle = document.getElementById("toggle");
-    const refresh = document.getElementById("refresh");
-    const theme = window.localStorage.getItem("theme");
-    
-    /* verifica se o tema armazenado no localStorage é escuro
-    se sim aplica o tema escuro ao body */
-    if (theme === "dark") document.body.classList.add("dark");
-    
-    // event listener para quando o botão de alterar o tema for clicado
-    toggle.addEventListener("click", () => {
-      document.body.classList.toggle("dark");
-      if (theme === "dark") {
-        window.localStorage.setItem("theme", "light");
-      } else window.localStorage.setItem("theme", "dark");
-    });
-    
-    refresh.addEventListener("click", () => {
-      window.location.reload();
-    });
-    
-    localStorage.setItem('theme', 'dark'); 
-    //acessado o tema da maquina do usuário
-    localStorage.getItem('theme'); 
-  </script>
 </head>
 
 <body>
@@ -110,6 +84,33 @@
       <a href="/login" class="icones-padrao"><i class="fa-solid fa-user" ></i></a>
       <button onclick id="toggle"><i class="fa-solid fa-circle-half-stroke"></i></button>
     </div>
+
+    {{-- Dark mode --}}
+  <script>
+    const toggle = document.getElementById("toggle");
+    const refresh = document.getElementById("refresh");
+    const theme = window.localStorage.getItem("theme");
+    
+    /* verifica se o tema armazenado no localStorage é escuro
+    se sim aplica o tema escuro ao body */
+    if (theme === "dark") document.body.classList.add("dark");
+    
+    // event listener para quando o botão de alterar o tema for clicado
+    toggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark");
+      if (theme === "dark") {
+        window.localStorage.setItem("theme", "light");
+      } else window.localStorage.setItem("theme", "dark");
+    });
+    
+    refresh.addEventListener("click", () => {
+      window.location.reload();
+    });
+    
+    localStorage.setItem('theme', 'dark'); 
+    //acessado o tema da maquina do usuário
+    localStorage.getItem('theme'); 
+  </script>
 
     {{-- <script>
       function darkMode() {
