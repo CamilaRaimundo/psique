a8a2639ea58e2f22b098b44a5bfab2590b9ddf2f
João Pedro Rahal <joao.rahal@unesp.br>
2023-10-20 08:45:47 -0300

    rotas final

--- psique_cti/resources/views/pages/contato.blade.php
+++ psique_cti/resources/views/pages/contato.blade.php
@@ -22,7 +22,7 @@
 
           <div class="input-group mb-3">
             <span class="input-group-text" id="inputGroup-sizing-default">Email:</span>
-            <input type="text" name="email" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+            <input type="text" name="email" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{Auth::user()->email}}" readonly>
           </div>
 
             <div class="input-group mb-3">
